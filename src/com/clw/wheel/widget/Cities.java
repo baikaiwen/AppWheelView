5e26a38dcb2488c95c4dfd58511e936f5b335d3e
baikaiwen <kaiwen.bai@cloudinward.com>
2016-10-11 14:22:31 +0800

    首次提交

---
+++ src/com/clw/wheel/widget/Cities.java
@@ -0,0 +1,27 @@
+package com.clw.wheel.widget;
+
+public class Cities {
+	private String _id;
+	private String name;
+	private String parent;
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getParent() {
+		return parent;
+	}
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+	
+	
+}
